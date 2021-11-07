827c0881fe1f88eb7bccbf7f6f00aa6cacd377b2
danfilof <danila.anfilofyev@gmail.com>
2021-11-07 13:32:56 +0100

    Tasks 5&6 done and the HW itself completed.

--- src/main/java/ru/gb/Cat.java
+++ src/main/java/ru/gb/Cat.java
@@ -1,24 +1,30 @@
 package ru.gb;
 
+
 public class Cat {
     private String name;
     private int appetite;
     private boolean fullness; // Дефолтное значение boolean - false, следовательно, по условию задачи, все созданные коты голодны.
 
+    public int getAppetite() {
+        return appetite;
+    }
 
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
+        this.fullness = false;
     }
 
     public boolean eat(Plate p) {
         if (appetite > p.getFood()) {
             System.out.println("В тарелке недостаточно еды для кота " + name + ".");
-            return fullness = false;
+            fullness = false;
         } else {
             p.decreaseFood(appetite);
-            return fullness = true;
+             fullness = true;
         }
+        return fullness;
     }
     public void printFullness () {
         if (fullness) {
@@ -27,4 +33,5 @@ public class Cat {
             System.out.println("Кот " + name + " остается голоден.");
         }
     }
+
 }
